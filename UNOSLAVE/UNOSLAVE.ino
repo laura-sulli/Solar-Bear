1a813da44ea20bc0c52cbf2d0ff4549f08779f73
laura-sulli <laurasulli143@gmail.com>
2021-03-10 12:58:51 -0800

    Update UNOSLAVE.ino

--- UNOSLAVE/UNOSLAVE.ino
+++ UNOSLAVE/UNOSLAVE.ino
@@ -1,30 +1,54 @@
-// Wire Slave Sender
-// by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Sends data as an I2C/TWI slave device
-// Refer to the "Wire Master Reader" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
+//Slave sender: Functionality: Will collect values from slave and send to the master
 
 #include <Wire.h>
+float solar;
+float battery; //might be uneccessary for these values 
+float crank;
+String solarvalue = "";
+String crankvalue = "";
+String batteryvalue = "";
+#define X 3 //arbitrary 
 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+  Wire.onRequest(voltmeterSolar); // address # 8 = solar values 
+  Wire.begin(9);               //address # 9 = battery values
+  Wire.onRequest(voltmeterBattery); // 
+  Wire.begin(10);               //address #10 = crank value 
+  Wire.onRequest(voltmeterCrank); 
 }
 
 void loop() {
   delay(100);
 }
 
+
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
-void requestEvent() {
+void voltmeterSolar() {
   //gather the value through the wattmeter
-  Wire.write("hello "); // respond with message of 6 bytes
+  solar = analogRead(X); //This aint it but it's something like that 
+  //transform into a strannnggg
+  solarvalue = String(solar); 
+  Wire.write(solarvalue); // respond with message of 6 bytes
   // as expected by master
+}
+
+void voltmeterBattery(){
+  //gather the value through the wattmeter
+  battery = analogRead(X); //This aint it but it's something like that 
+  //transform into a strannnggg
+  batteryvalue = String(battery); 
+  Wire.write(batteryvalue); // respond with message of 6 bytes
+  // as expected by master
+}
+
+void voltmeterCrank(){
+  //gather the value through the wattmeter
+  crank = analogRead(X); //This aint it but it's something like that 
+  //transform into a strannnggg
+  crankvalue = String(crank); 
+  Wire.write(crankvalue); // respond with message of 6 bytes
+  // as expected by master
+  
 }
